bd3d21f3daf876f9f5a1a0765ce06720aff983fc
mtemnov <michael.temnov@students.uni-mannheim.de>
2023-09-26 12:48:15 +0200

    Initial custom implementation of basic file events and handlers

---
+++ custom_impl/client/client.py
@@ -0,0 +1,152 @@
+import os
+import time
+import json
+import socket
+import difflib
+import base64
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# Import server configuration
+# Load configuration from the JSON file
+with open('client_config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+# Use the loaded configuration
+SERVER_HOST = config["server_host"]
+SERVER_PORT = config["server_port"]
+CLIENT_DIR = config["server_dir"]
+
+# Keep track of recently created/modified files to catch the successive modified event thrown by watchdog
+recently_changed_files = []
+
+class SyncHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+
+        if event.src_path in recently_changed_files:
+            recently_changed_files.remove(event.src_path)
+            return
+
+        """ # NOT WORKINGidea to use difflib to only save changes
+        # however, a storage of the original data would need to be done beforehand
+        relative_path = os.path.relpath(event.src_path, CLIENT_DIR)
+        original_file_path = event.src_path + ".orig"
+    
+        # Calculate differences between the original and modified files
+        with open(original_file_path, 'r', encoding='utf-8') as orig_file:
+            orig_content = orig_file.readlines()
+
+        with open(event.src_path, 'rb') as mod_file:
+            mod_content = mod_file.readlines()
+
+        d = difflib.Differ()
+        diff = list(d.compare(orig_content, mod_content))
+        diff_str = '\n'.join(diff)
+
+        # Include the differences in the synchronization message
+        message = {
+            "action": "update",
+            "path": relative_path,
+            "event_type": "modified",
+            "data": diff_str  # Include the differences
+        }
+
+        # Update the original file with the modified content
+        with open(original_file_path, 'w', encoding='utf-8') as orig_file:
+            orig_file.writelines(mod_content)
+
+        send_message_to_server(message)"""
+
+        # Current workaround to just completely overwrite the file when it is modified
+        relative_path = os.path.relpath(event.src_path, CLIENT_DIR)
+
+        # Read the content of the modified file
+        modified_content = self.read_bytes(event.src_path)
+
+        # Encode the modified content as Base64 and send it to the server
+        data_base64 = base64.b64encode(modified_content).decode('utf-8')
+
+        message = {
+            "action": "update",
+            "path": relative_path,
+            "event_type": "modified",
+            "data": data_base64
+        }
+        recently_changed_files.append(event.src_path)
+        send_message_to_server(message)
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            return
+        relative_path = os.path.relpath(event.src_path, CLIENT_DIR)
+        message = {
+            "action": "update",
+            "path": relative_path,
+            "event_type": "deleted"
+        }
+        send_message_to_server(message)
+
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        relative_path = os.path.relpath(event.src_path, CLIENT_DIR)
+        
+        # Read the content of the created file as bytes
+        data_bytes = self.read_bytes(event.src_path)
+        
+        # Encode the binary data as Base64
+        data_base64 = base64.b64encode(data_bytes).decode('utf-8')
+        
+        message = {
+            "action": "update",
+            "path": relative_path,
+            "event_type": "created",
+            "data": data_base64
+        }
+        recently_changed_files.append(event.src_path)
+        send_message_to_server(message)
+
+    def on_moved(self, event):
+        if event.is_directory:
+            return
+        src_relative_path = os.path.relpath(event.src_path, CLIENT_DIR)
+        dest_relative_path = os.path.relpath(event.dest_path, CLIENT_DIR)
+        message = {
+            "action": "update",
+            "src_path": src_relative_path,
+            "dest_path": dest_relative_path,
+            "event_type": "moved"
+        }
+        send_message_to_server(message)
+
+    def read_bytes(self, file_path):
+        with open(file_path, 'rb') as file:
+            return file.read()
+
+def send_message_to_server(message):
+    print(message)
+    # Create a socket connection to the server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        try:
+            client_socket.connect((SERVER_HOST, SERVER_PORT))
+            # Serialize the message to JSON
+            message_json = json.dumps(message)
+            # Send the message to the server
+            client_socket.sendall(message_json.encode())
+        except ConnectionError as e:
+            print(f"Error connecting to server: {e}")
+
+if __name__ == "__main__":
+    event_handler = SyncHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=CLIENT_DIR, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
---
+++ custom_impl/server/server.py
@@ -0,0 +1,110 @@
+import os
+import json
+import socket
+from threading import Thread
+import difflib
+import base64
+
+# Import server configuration
+# Load configuration from the JSON file
+with open('server_config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+# Use the loaded configuration
+SERVER_HOST = config["server_host"]
+SERVER_PORT = config["server_port"]
+SERVER_DIR = config["server_dir"]
+
+def handle_client(client_socket):
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+
+            # Deserialize the received message
+            message = json.loads(data.decode())
+
+            if message["action"] == "update":
+                print(message["event_type"])
+                handle_update(message)
+
+    except json.JSONDecodeError:
+        print("Error decoding JSON message")
+    finally:
+        client_socket.close()
+
+def handle_update(message):
+    if message["event_type"] == "modified":
+        # Handle file modification event with differences
+        server_path = os.path.join(SERVER_DIR, message["path"])
+        
+        """ CURRENTLY NOT WORKING
+        # Read the current content of the server file
+        with open(server_path, 'r', encoding='utf-8') as server_file:
+            server_content = server_file.readlines()
+        
+        # Decode the Base64-encoded data received from the client
+        client_data_base64 = message["data"]
+        client_data_bytes = base64.b64decode(client_data_base64)
+        
+        # Decode the client data as UTF-8 text
+        client_data = client_data_bytes.decode('utf-8')
+        
+        # Apply the differences to the server content
+        d = difflib.Differ()
+        diff = list(d.compare(server_content, client_data.splitlines(keepends=True)))
+        
+        # Update the server file with the modified content
+        with open(server_path, 'w', encoding='utf-8') as server_file:
+            server_file.writelines(diff)"""
+        
+        # Temporarily use the same logic as for file creation
+        # Handle file creation event
+        server_path = os.path.join(SERVER_DIR, message["path"])
+        
+        # Decode the Base64-encoded data received from the client
+        client_data_base64 = message["data"]
+        client_data_bytes = base64.b64decode(client_data_base64)
+        
+        # Write the client data to the server file
+        with open(server_path, 'wb') as server_file:
+            server_file.write(client_data_bytes)
+
+    elif message["event_type"] == "deleted":
+        # Handle file deletion event
+        server_path = os.path.join(SERVER_DIR, message["path"])
+        if os.path.exists(server_path):
+            os.remove(server_path)
+
+    elif message["event_type"] == "created":
+        # Handle file creation event
+        server_path = os.path.join(SERVER_DIR, message["path"])
+        
+        # Decode the Base64-encoded data received from the client
+        client_data_base64 = message["data"]
+        client_data_bytes = base64.b64decode(client_data_base64)
+        
+        # Write the client data to the server file
+        with open(server_path, 'wb') as server_file:
+            server_file.write(client_data_bytes)
+
+    elif message["event_type"] == "moved":
+        # Handle file move/rename event
+        src_server_path = os.path.join(SERVER_DIR, message["src_path"])
+        dest_server_path = os.path.join(SERVER_DIR, message["dest_path"])
+        if os.path.exists(src_server_path):
+            # You may want to implement logic to move/rename the server file here
+            os.rename(src_server_path, dest_server_path)
+
+if __name__ == "__main__":
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
+    server_socket.listen(5)
+    print(f"Server listening on {SERVER_HOST}:{SERVER_PORT}")
+
+    while True:
+        client_socket, _ = server_socket.accept()
+        print(f"Accepted connection from {client_socket.getpeername()}")
+        client_handler = Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
